6ed647afad1e17765524cf909d64756452abc1b6
javierbyte <hi@javierbyte.com>
2015-07-04 17:52:30 -0500

    Update deps

--- dist/app.js
+++ dist/app.js
@@ -1,112 +1,114 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
-var _objectWithoutProperties = function (obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; };
-
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
-var React = require('react/addons'),
-    stackBlurImage = require('../lib/StackBlur.js');
+function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
+
+var React = require('react/addons');
+var stackBlurImage = require('../lib/StackBlur.js');
 
 var ReactBlur = React.createClass({
-    displayName: 'ReactBlur',
+  displayName: 'ReactBlur',
 
-    mixins: [React.addons.PureRenderMixin],
+  mixins: [React.addons.PureRenderMixin],
 
-    propTypes: {
-        img: React.PropTypes.string.isRequired,
-        blurRadius: React.PropTypes.number,
-        resizeInterval: React.PropTypes.number
-    },
+  propTypes: {
+    img: React.PropTypes.string.isRequired,
+    blurRadius: React.PropTypes.number,
+    resizeInterval: React.PropTypes.number,
+    className: React.PropTypes.string,
+    children: React.PropTypes.any
+  },
 
-    getDefaultProps: function getDefaultProps() {
-        return {
-            blurRadius: 0,
-            resizeInterval: 128
-        };
-    },
+  getDefaultProps: function getDefaultProps() {
+    return {
+      blurRadius: 0,
+      resizeInterval: 128
+    };
+  },
 
-    componentDidMount: function componentDidMount() {
-        var _this = this;
+  componentDidMount: function componentDidMount() {
+    var _this = this;
 
-        window.addEventListener('resize', this.resize);
+    var blurRadius = this.props.blurRadius;
 
-        var blurRadius = this.props.blurRadius;
+    var container = React.findDOMNode(this);
 
-        var container = React.findDOMNode(this);
+    this.height = container.offsetHeight;
+    this.width = container.offsetWidth;
 
-        this.height = container.offsetHeight;
-        this.width = container.offsetWidth;
+    this.canvas = React.findDOMNode(this.refs.canvas);
+    this.canvas.height = this.height;
+    this.canvas.width = this.width;
 
-        this.canvas = React.findDOMNode(this.refs.canvas);
-        this.canvas.height = this.height;
-        this.canvas.width = this.width;
+    this.img = new Image();
+    this.img.crossOrigin = 'Anonymous';
+    this.img.onload = function () {
+      stackBlurImage(_this.img, _this.canvas, blurRadius, _this.width, _this.height);
+    };
+    this.img.src = this.props.img;
 
-        var ctx = this.canvas.getContext('2d');
-        this.img = new Image();
-        this.img.crossOrigin = 'Anonymous';
-        this.img.onload = function () {
-            stackBlurImage(_this.img, _this.canvas, blurRadius, _this.width, _this.height);
-        };
-        this.img.src = this.props.img;
-    },
+    window.addEventListener('resize', this.resize);
+  },
 
-    componentWillUnmount: function componentWillUnmount() {
-        window.removeEventListener('resize', this.resize);
-    },
+  componentWillUnmount: function componentWillUnmount() {
+    window.removeEventListener('resize', this.resize);
+  },
 
-    resize: function resize() {
-        var _this2 = this;
+  resize: function resize() {
+    var _this2 = this;
 
-        var now = +new Date(),
-            args = arguments,
-            deferTimer,
-            threshhold = this.props.resizeInterval;
+    var now = new Date().getTime();
+    var deferTimer;
+    var threshhold = this.props.resizeInterval;
 
-        if (this.last && now < this.last + threshhold) {
-            clearTimeout(deferTimer);
-            deferTimer = setTimeout(function () {
-                _this2.last = now;
-                _this2.doResize();
-            }, threshhold);
-        } else {
-            this.last = now;
-            this.doResize();
-        }
-    },
-
-    doResize: function doResize() {
-        var container = React.findDOMNode(this);
-
-        this.height = container.offsetHeight;
-        this.width = container.offsetWidth;
-
-        stackBlurImage(this.img, this.canvas, this.props.blurRadius, this.width, this.height);
-    },
-
-    componentWillReceiveProps: function componentWillReceiveProps(nextProps) {
-        stackBlurImage(this.img, this.canvas, nextProps.blurRadius, this.width, this.height);
-    },
-
-    render: function render() {
-        var _props = this.props;
-        var img = _props.img;
-        var className = _props.className;
-        var children = _props.children;
-        var other = _objectWithoutProperties(_props, ['img', 'className', 'children']);
-        var classes = 'react-blur';
-
-        if (className) {
-            classes += ' ' + className;
-        }
-
-        return React.createElement(
-            'div',
-            _extends({}, other, { className: classes, onClick: this.clickTest }),
-            React.createElement('canvas', { className: 'react-blur-canvas', ref: 'canvas' }),
-            children
-        );
+    if (this.last && now < this.last + threshhold) {
+      clearTimeout(deferTimer);
+      deferTimer = setTimeout(function () {
+        _this2.last = now;
+        _this2.doResize();
+      }, threshhold);
+    } else {
+      this.last = now;
+      this.doResize();
     }
+  },
+
+  doResize: function doResize() {
+    var container = React.findDOMNode(this);
+
+    this.height = container.offsetHeight;
+    this.width = container.offsetWidth;
+
+    stackBlurImage(this.img, this.canvas, this.props.blurRadius, this.width, this.height);
+  },
+
+  componentWillUpdate: function componentWillUpdate(nextProps) {
+    stackBlurImage(this.img, this.canvas, nextProps.blurRadius, this.width, this.height);
+  },
+
+  render: function render() {
+    var _props = this.props;
+    var img = _props.img;
+    var className = _props.className;
+    var children = _props.children;
+
+    var other = _objectWithoutProperties(_props, ['img', 'className', 'children']);
+
+    var classes = 'react-blur';
+
+    if (className) {
+      classes += ' ' + className;
+    }
+
+    return React.createElement(
+      'div',
+      _extends({}, other, { className: classes, onClick: this.clickTest }),
+      React.createElement('canvas', { className: 'react-blur-canvas', ref: 'canvas' }),
+      children
+    );
+  }
 });
 
 module.exports = ReactBlur;
@@ -22264,7 +22266,7 @@ process.nextTick = function (fun) {
         }
     }
     queue.push(new Item(fun, args));
-    if (!draining) {
+    if (queue.length === 1 && !draining) {
         setTimeout(drainQueue, 0);
     }
 };
